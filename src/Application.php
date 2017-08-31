700b38242ad069c0a5684211e1b3de61ca179d06
Martin Ostrat <martin.ostrat@khk.ee>
2017-08-31 10:27:27 +0300

    Register new form services, create a form object and render is in a new form template

--- src/Application.php
+++ src/Application.php
@@ -4,7 +4,18 @@ namespace BookingApp;
 
 use Silex\Application as SilexApplication;
 use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\LocaleServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 /**
  * Custom Application class that hold our application specifix functionality.
@@ -38,9 +49,14 @@ class Application extends SilexApplication
                 'path' => __DIR__.'/../database/app.db',
             ],
         ]);
+
+        $this->register(new FormServiceProvider());
+        $this->register(new LocaleServiceProvider());
+        $this->register(new TranslationServiceProvider(), [
+            'translator.domains' => [],
+        ]);
     }
 
-        // Creating a table if it doesn't exist yet
     /**
      * Creates all needed tables to database if they don't exist.
      */
@@ -49,6 +65,16 @@ class Application extends SilexApplication
         if (!$this['db']->getSchemaManager()->tablesExist('bookings')) {
             $this['db']->executeQuery("CREATE TABLE bookings (
                 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                firstName VARCHAR(40) NOT NULL,
+                lastName VARCHAR(40) NOT NULL,
+                phone VARCHAR(10) NOT NULL,
+                email VARCHAR(20) DEFAULT NULL,
+                birthday DATE NOT NULL,
+                startDate DATE NOT NULL,
+                endDate DATE NOT NULL,
+                arrivalTime TIME DEFAULT NULL,
+                additionalInformation TEXT,
+                nrOfPeople INT NOT NULL,
                 payingMethod VARCHAR(10) NOT NULL
             );");
         }
@@ -60,7 +86,47 @@ class Application extends SilexApplication
     private function configureControllers()
     {
         $this->get('/bookings/create', function () {
-            return $this['twig']->render('base.html.twig');
+
+            $form = $this['form.factory']->createBuilder(FormType::class)
+                ->add('firstName', TextType::class, ['required' => true])
+                ->add('lastName', TextType::class, ['required' => true])
+                ->add('phone', TextType::class, ['required' => true])
+                ->add('email', TextType::class, ['required' => false])
+                ->add('birthday', DateType::class, [
+                    'required' => true,
+                    'widget' => 'single_text',
+                    'format' => 'dd.MM.yyyy',
+                ])
+                ->add('startDate', DateType::class, [
+                    'required' => true,
+                    'widget' => 'single_text',
+                    'format' => 'dd.MM.yyyy',
+                ])
+                ->add('endDate', DateType::class, [
+                    'required' => true,
+                    'widget' => 'single_text',
+                    'format' => 'dd.MM.yyyy',
+                ])
+                ->add('arrivalTime', TimeType::class, ['required' => true])
+                ->add('nrOfPeople', IntegerType::class, ['required' => true])
+                ->add('payingMethod', ChoiceType::class, [
+                    'choices' => [
+                        'cash' => 'cash',
+                        'transfer' => 'transfer',
+                    ],
+                    'required' => true,
+                ])
+                ->add('additionalInformation', TextareaType::class, [
+                    'required' => false
+                ])
+                ->add('submit', SubmitType::class, ['label' => 'Send booking'])
+                ->getForm()
+            ;
+
+            return $this['twig']->render('form.html.twig', [
+                'form' => $form->createView()
+            ]);;
         });
     }
 }
+
