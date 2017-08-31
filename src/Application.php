6c7af6437372edc90dd8d9fe3a3cb9fcfc45a8cf
Martin Ostrat <martin.ostrat@khk.ee>
2017-08-31 11:23:18 +0300

    Have a separate url for seeing submitted bookings

--- src/Application.php
+++ src/Application.php
@@ -3,6 +3,7 @@
 namespace BookingApp;
 
 use BookingApp\Controllers\CreateBookingController;
+use BookingApp\Controllers\ListBookingsController;
 use Silex\Application as SilexApplication;
 use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\FormServiceProvider;
@@ -81,8 +82,14 @@ class Application extends SilexApplication
         $this
             ->match('/bookings/create', new CreateBookingController(
                 $this['form.factory'],
-                $this['twig']
+                $this['twig'],
+                $this['db']
             ))
+            ->method('GET|POST')
+        ;
+
+        $this
+            ->get('/bookings', new ListBookingsController($this['db'], $this['twig']))
         ;
     }
 }
